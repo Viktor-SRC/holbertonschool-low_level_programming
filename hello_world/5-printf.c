d21624fc4bddd3a56246a7f1ab63836cc7d9b219
Viktor-SRC <vk.src9@gmail.com>
2023-10-21 04:48:04 -0700

    5.3

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,15 +1,15 @@
 /**
- *main - Prints "with proper grammar, but the outcome is a piece of art,"
- *
- *Description: Uses the printf function from the stdio.h library to print
- *"with proper grammar, but the outcome is a piece of art," followed by a new line
- *
- *Return: 0 if successful, non-zero otherwise
- */
+*main - Prints "with proper grammar, but the outcome is a piece of art,"
+*
+*Description: Uses the printf function from the stdio.h library to print
+*"with proper grammar, but the outcome is a piece of art," flloed by a new lin
+*
+* *Return: 0 if successful, non-zero otherwise
+*/
 #include <stdio.h>
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a  piece of art,");
 return (0);
 }
