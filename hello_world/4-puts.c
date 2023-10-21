8a7429fc750a6b263f221c537317c189cfb88e20
Viktor-SRC <vk.src9@gmail.com>
2023-10-21 03:51:35 -0700

    4.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * puts - This is a description
+  * puts - This is a description
  */
-
-int main() 
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
