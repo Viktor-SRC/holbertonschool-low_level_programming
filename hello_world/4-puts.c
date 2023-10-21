3772e570149ff1f22e38f3101a8774d285200d04
Viktor-SRC <vk.src9@gmail.com>
2023-10-21 04:10:03 -0700

    4.8

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
 /**
-* puts - prints "Programming is like building a multilingual puzzle
-* return 0
-*/
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Description: Uses the puts function from the stdio.h library to print
+ * "Programming is like building a multilingual puzzle" followed by a new line.
+ * The function then returns 0 to indicate successful execution.
+ *
+ * Return: 0 if successful, non-zero otherwise
+ */
+#include <stdio.h>
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
