6657c0825c7c76a7e75ffe4767d627cc73842912
Viktor-SRC <vk.src9@gmail.com>
2023-10-22 22:32:29 -0700

    1.3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
 *main - 1. The last digit
-*Description function to print last digit of a random number 
+*Description function to print last digit of a random number
 *Return: Always 0 (Success)
 */
 int main(void)
@@ -21,7 +21,8 @@ int main(void)
 	else if (last_digit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit); 	
 	else
-		printf("Last digit on %d id %d and is lees than 6 and not 0\n", n, last_digit);
+		printf("Last digit on %d is %d and is less than 6 and not 0\n",
+			n, last_digit);
 
 	return (0);
 }
