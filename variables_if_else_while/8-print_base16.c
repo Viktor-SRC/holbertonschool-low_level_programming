435e44ee7bd57503e4f1743259c5952277249c2f
Viktor-SRC <vk.src9@gmail.com>
2023-10-27 16:16:36 -0700

    8.3

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,5 +1,5 @@
 /**
- * main 8. Hexadecimal.
+ * main - 8. Hexadecimal.
  * Description, prints all the numbers of base 16
  * in lowercase, followed by a new line
  * Return: Always 0 (Success)
@@ -10,11 +10,11 @@
 int main(void)
 {
 	int n;
-	
+
 	for (n = 0; n < 16; n++)
 	{
-		if (n<10)
-			putchar('0' + n );
+		if (n < 10)
+			putchar('0' + n);
 		else
 			putchar('a' + n - 10);
 	}
