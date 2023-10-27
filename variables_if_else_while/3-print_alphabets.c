3f4e33c4887f30ad1ab0f5a73ac01af5acfd5975
Viktor-SRC <vk.src9@gmail.com>
2023-10-27 11:31:23 -0700

    3.1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+/**
+ * main - 3. alphABET.
+ * Description prints tha alphabet in lowercase and uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <='z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
