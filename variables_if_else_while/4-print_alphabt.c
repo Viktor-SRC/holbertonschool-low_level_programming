10015aa7592ce1b71407b022fea49566ba010c02
Viktor-SRC <vk.src9@gmail.com>
2023-10-27 13:52:21 -0700

    4.1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/**
+ * main -  4. When I was having that alphabet soup.
+ * Description function to prints the alphabet in lowercase,
+ * except 'q' and 'e'
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+        char ch;
+
+        for (ch = 'a' ; ch <= 'z' ; ch++)
+        {
+	if (ch != 'q' && ch != 'e')
+	{
+                putchar(ch);
+        }
+	}
+        putchar('\n');
+        return (0);
+}
