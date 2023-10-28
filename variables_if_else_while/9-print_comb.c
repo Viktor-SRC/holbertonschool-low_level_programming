ee20b6a5bd7e3e27c6d8a1028c385e35e94a5e31
Viktor-SRC <vk.src9@gmail.com>
2023-10-27 17:04:59 -0700

    9.3

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-			putchar('0' + i);
-			if (i < 9)
+		putchar('0' + i);
+		if (i < 9)
 	{
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 	}
 	}
 	putchar('\n');
