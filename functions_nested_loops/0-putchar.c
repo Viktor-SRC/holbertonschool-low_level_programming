ea0c367753fdac508293ebf6ea6524267260e928
Viktor-SRC <vk.src9@gmail.com>
2023-10-31 15:32:26 -0700

    0.1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/**
+ *main - 0 putchar
+ *Description: program to print _putchar
+ *Return: Always 0 (Success)
+ */
+
+#include "main.h"
+
+int main(void)
+{
+	char abc[] = "_putchar";
+	int i;
+
+	for (i = 0; i = 8; i++)
+	{
+		_putchar(abc[i]);
+	}
+
+	{
+		_putchar('\n');
+		return (0);
+	}
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory isw about C programs of C - Functions, nested loops project
