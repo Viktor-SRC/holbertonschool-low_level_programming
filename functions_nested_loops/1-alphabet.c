4255398be1167e03c02d65e93a39ab4c8cf4e049
Viktor-SRC <vk.src9@gmail.com>
2023-10-31 16:34:03 -0700

    1.1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - 1 I sometimes suffer from insomnia.
+ * Description: program to print the alphabet
+ * in lowercase
+ * Return: Always 0 (Success)
+ */
+
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char abc[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; 1 < 26; i++)
+	{
+		_putchar(abc[i]);
+	}
+		_putchar('\n');
+}
