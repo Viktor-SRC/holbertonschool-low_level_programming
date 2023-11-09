b6aaf19c973ad04f6c6b05ab14e16542b0e84949
Viktor-SRC <vk.src9@gmail.com>
2023-11-09 01:34:17 -0800

    11.1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+/**
+ * print_to_98 - 11 Battery Street.
+ * Description: function to prints all natural numbers from
+ * n to 98
+ * @n: first number to print
+ *
+ * return: void
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+				if (n != 98)
+				{
+					printf(", ");
+				}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("d%", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
