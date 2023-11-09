278205be17d49aaa1c8f726de375770431f5dd50
Viktor-SRC <vk.src9@gmail.com>
2023-11-08 21:47:44 -0800

    4.1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/**
+ * _isalpha - 4 is alpha.
+ * Description: function to check for alphabetic character
+ * @c: the character to check
+ *
+ * Return: 1 if the character is a letter
+ * 0 otherwise
+ */
+
+#include "main.h"
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
