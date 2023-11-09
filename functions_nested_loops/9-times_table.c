59713d83362ad7383465907963c96204c07b0074
Viktor-SRC <vk.src9@gmail.com>
2023-11-09 00:28:12 -0800

    9.2

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,27 +9,27 @@
 
 void times_table(void)
 {
-	int i,j;
+	int i, j;
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			int product = i * j;
-			if (product < 10)
+			if (j != 0)
 			{
-				_putchar('0' + product);
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
 			}
+			if (product < 10)
+				_putchar('0' + product);
 			else
 			{
 		_putchar('0' + product / 10);
 		_putchar('0' + product % 10);
 			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 		}
 		_putchar('\n');
-}
+	}
 }
