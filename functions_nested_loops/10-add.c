2884a26197113bee9f14d4abc887d8ba92865780
Viktor-SRC <vk.src9@gmail.com>
2023-11-09 00:59:33 -0800

    10.1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - 10 a+b.
+ * Description: function to adds two integers and returns the result.
+ * @i: the first integer to add
+ * @j: the second integer to add
+ * Return: (i + j)
+ */
+
+#include "main.h"
+
+int add(int i, int j)
+{
+	return (i + j);
+}
