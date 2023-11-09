8edd1cac54b2b16fe7157a3b04dd010b2952bc5c
Viktor-SRC <vk.src9@gmail.com>
2023-11-08 22:36:46 -0800

    6.1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+ * _abs - 6 abs.
+ * Description: function to computes tha absolute value of an integer.
+ * Return: always 0 
+ */
+
+#include "main.h"
+
+int _abs(int)
+{
+	int n;
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
