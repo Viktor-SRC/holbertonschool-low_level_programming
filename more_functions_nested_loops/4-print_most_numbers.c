299f854614994563a982b80074dc3a7441463190
Viktor-SRC <vk.src9@gmail.com>
2023-11-20 21:58:45 -0800

    4.1

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+/**
+ *print_numbers - function to print numbers from 0 to 9
+ *Description: print from 0 to 9
+ *return: void
+ */
+
+#include "main.h"
+
+void print_numbers(void)
+{
+        char numbers[] = "01356789";
+        int i;
+
+        for (i = 0; i <= 7; i++)
+        {
+                _putchar(numbers[i]);
+        }
+                _putchar('\n');
+}
