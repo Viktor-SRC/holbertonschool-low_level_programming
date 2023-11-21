f879699517d2008baa1a1a745c7a43db39aa412f
Viktor-SRC <vk.src9@gmail.com>
2023-11-20 21:11:45 -0800

    0.1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - function 
+ * Description: check if character is uppercase or not
+ * @c: is a character to be evaluated
+ * Return 1 if uppercase else 0 otherwise
+ */
+
+#include "main.h"
+
+int _isupper(int c)
