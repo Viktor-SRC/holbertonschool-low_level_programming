a2cad0e62740e6c151ee747c17e12bf2fc8a5a53
Viktor-SRC <vk.src9@gmail.com>
2023-11-20 21:53:25 -0800

    3.2

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,9 @@
+/**
+ *print_numbers - function to print numbers from 0 to 9
+ *Description: print from 0 to 9
+ *return: void
+ */
+
 #include "main.h"
 
 void print_numbers(void)
@@ -5,7 +11,7 @@ void print_numbers(void)
 	char numbers[] = "0123456789";
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(numbers[i]);
 	}
