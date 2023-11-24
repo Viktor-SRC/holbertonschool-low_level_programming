ac2425178407dede91f4a3063257682acaed0d11
Viktor-SRC <vk.src9@gmail.com>
2023-11-23 18:04:30 -0800

    9.6

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,27 @@
-/**
- * main - function to print numbers
- * Description: Fizz-Buzz test
- *
- * Return: Always 0
- */
-
+#include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Fizz Buzz
+ * Description - print fizz for multiples of 3 or buzz in multiples of 5
+ * Return: Always 0
+ */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	for (; i < 100 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
-		else if (i == 100)
-		{
-			printf("Buzz\n");
-		}
 		else
-		{
 			printf("%d ", i);
-		}
 	}
+	printf("Buzz\n");
+
 	return (0);
 }
