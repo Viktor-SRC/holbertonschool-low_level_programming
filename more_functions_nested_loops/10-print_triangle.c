6defb433f5585f72f250564c4c5c54854910b90a
Viktor-SRC <vk.src9@gmail.com>
2023-11-23 19:20:22 -0800

    10.6

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,17 +1,30 @@
-include "main.h"
+#include "main.h"
 
-void print_triangle(int)
+/**
+ * print_triangle -  prints triangle
+ * Description - prints a triangle for size n
+ * @size: An input integer
+ * Return: Always 0
+ */
+void print_triangle(int size)
 {
-	int i, j, k;
-    for (i = 0; i < size; i++) 
-    {
-        for (k = 0; k < size - i - 1; k++) 
+	int i = 0, j, n = size - 1;
+
+	if (size > 0)
 	{
-            printf(" ");
-        }
-        for (j = 0; j <= i; j++) {
-            printf("#");
-        }
-        printf("\n");
-    }
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
