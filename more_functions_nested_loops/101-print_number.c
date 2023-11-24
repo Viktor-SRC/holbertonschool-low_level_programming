1d72fbb7a04a1531f05ea99a40033a2fcb31edb8
Viktor-SRC <vk.src9@gmail.com>
2023-11-23 19:25:20 -0800

    12.1

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void print_number(int n)
+{
+    unsigned int number;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        number = -n;
+    }
+    else
+    {
+        number = n;
+    }
+
+    if (number / 10)
+    {
+        print_number(number / 10);
+    }
+
+    _putchar((number % 10) + '0');
+}
+
