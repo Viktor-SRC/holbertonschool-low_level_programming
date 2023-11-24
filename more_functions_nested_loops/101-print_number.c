413999a4ba314b26dc544603295b12d08be6207e
Viktor-SRC <vk.src9@gmail.com>
2023-11-23 19:29:37 -0800

    12.2

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,26 @@
+/**
+ * print_number - print number
+ * Description - print number
+ * Return: always 0
+ */
+
 #include "main.h"
 
 void print_number(int n)
 {
-    unsigned int number;
-
-    if (n < 0)
-    {
-        _putchar('-');
-        number = -n;
-    }
-    else
-    {
-        number = n;
-    }
-
-    if (number / 10)
-    {
-        print_number(number / 10);
-    }
-
-    _putchar((number % 10) + '0');
+	unsigned int number;
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -n;
+	}
+	else
+	{
+		number = n;
+	}
+	if (number / 10)
+	{
+		print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
 }
-
