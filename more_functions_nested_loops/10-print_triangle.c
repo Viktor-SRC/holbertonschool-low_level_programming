5a6165f2c49c0495371733bcf49df5c7339c1d03
Viktor-SRC <vk.src9@gmail.com>
2023-11-23 18:57:18 -0800

    10.4

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,12 +2,16 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
+			for(k = 0; k < size - i - 1; k++)
+			{
+				_putchar(' ');
+			}
 			for(j = 0; j <= i; j++)
 			{
 				_putchar('#');
@@ -15,9 +19,6 @@ void print_triangle(int size)
 			
 				_putchar('\n');
 			}
-		}
-	else
-		{
 			_putchar('\n');
 		}
 	}
