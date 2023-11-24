6ffaca41b2cb46a33e01fd1050171267be665b8c
Viktor-SRC <vk.src9@gmail.com>
2023-11-23 19:34:39 -0800

    11.1

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef long long int ll;
+
+ll maxPrimeFactors(ll n) {
+    ll maxPrime = -1;
+
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n >>= 1; // equivalent to n /= 2
+    }
+
+    // n must be odd at this point, thus skip the even numbers and iterate only for odd integers
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+    }
+
+    // This condition is to handle the case when n is a prime number greater than 2
+    if (n > 2)
+        maxPrime = n;
+
+    return maxPrime;
+}
+
+int main() {
+    ll n = 612852475143;
+    printf("%lld\n", maxPrimeFactors(n));
+    return 0;
+}
+
